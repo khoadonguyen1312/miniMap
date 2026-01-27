f7f1a12d6034734738f900543e2c099484dfdddf
khoadonguyen1312 <khoadonguyen8@gmail.com>
2026-01-27 15:48:57 +0700

    a

--- lib/config/customTheme.dart
+++ lib/config/customTheme.dart
@@ -8,6 +8,3 @@ ThemeData lightmode = ThemeData(
   brightness: Brightness.light,
   // scaffoldBackgroundColor: AppColors.background,
 );
-
-const String apiKey =
-    "pk.eyJ1Ijoia2hvYWRvbmd1eWVuMTMxMiIsImEiOiJjbWtmMjc3dmkwZXRlM2NxMDAwcWx4cGNsIn0.m9NTBqb9YzxcuLHVZ4LaEw";
